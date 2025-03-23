c2fbebc509907b29a6e2812ae5bb3a6f173af67f
SureshKumar-U <kumarsk12072000@gmail.com>
2025-03-24 00:05:35 +0530

    client added

---
+++ client/src/app/upload/page.js
@@ -0,0 +1,17 @@
+
+
+
+import UploadForm from '../../components/uploadform';
+
+
+const UploadPage = () => {
+ return (
+   <div className='m-10'>
+     <h1>HHLD YouTube - Upload Page</h1>
+     <UploadForm/>
+   </div>
+ );
+};
+
+
+export default UploadPage;
